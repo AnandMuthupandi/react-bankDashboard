6a7d2bd38697de60612e101295acc440dbbc32bb
anand91m <anand91m@gmail.com>
2023-09-07 12:20:40 +0200

    add mainlayout in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from "react";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import { routes } from "./routes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          {routes}
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
